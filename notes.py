785d5f49415fe1193a9743eae1c612c137ec34b2
Romyyy0x786 <124658279+Romyyy0x786@users.noreply.github.com>
2024-06-24 07:33:35 -0700

    Add files via upload

---
+++ notes.py
@@ -0,0 +1,14 @@
+lista = ["azul", "amarelo"]
+print (lista)
+
+lista.append("verde")
+
+print(lista)
+
+tupla = ("azul" , "amarelo")
+print(tupla)
+
+dict = {""}
+
+conjunto = {"vermelho", "azul", "verde"} #sem ordenaçao, nao tem indice
+print (conjunto)
